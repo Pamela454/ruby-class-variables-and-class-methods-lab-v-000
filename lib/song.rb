853126f39a57dcee3997579e992a071474a5535d
Pamela Frieze <pfrieze@gmail.com>
2017-11-14 17:00:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,13 @@
 class Song
   attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = 
+  @@genres = 
 
 def initialize(name, artist, genre)
   @name = name
   @artist = artist
   @genre = genre
 end
-
-  @@count = 0
-  @@artists = 
-  @@genres = 
+ 
 end
