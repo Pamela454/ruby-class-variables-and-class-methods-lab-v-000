8926c8bcf20e9725ff076641817d73b5b9723a8d
Pamela Frieze <pfrieze@gmail.com>
2017-11-14 17:56:22 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,10 +39,10 @@ end
 
 def self.artist_count
   artist_count = {}
-  @@genres.each do |genre|
-    if genre_count[genre]
-      genre_count[genre] += 1
-    else genre_count[genre] = 1
+  @@artist.each do |artist|
+    if artist_count[artist]
+      artist_count[artist] += 1
+    else artist_count[artist] = 1
     end
   end
   artist_count
